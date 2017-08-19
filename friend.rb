bfa7cb2f296f30a4f497fd357aba9f104011f52f
natail <nataleanfuso@gmail.com>
2017-08-19 13:39:31 -0500

    solved friends, solvingfor words with a length of 4 given an array

---
+++ friend.rb
@@ -0,0 +1,19 @@
+#Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+#If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours!
+
+#Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+
+
+def friend(friends)
+    my_friends = []
+
+    friends.each do |friend|
+        if friend.length == 4
+            my_friends << friend
+        end
+    end
+    p my_friends 
+end
+
+friend(["Ryan", "Kieran", "Mark"])
